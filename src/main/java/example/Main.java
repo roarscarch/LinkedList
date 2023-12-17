556a2bcf6d78a3ea6cb489c668ee74154c814ae8
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-17 23:47:03 +0530

    Simple LL created

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -2,6 +2,16 @@ package example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to LL Class");
+            Node<Integer> node1 = new Node<>(56);
+            Node<Integer> node2 = new Node<>(30);
+            Node<Integer> node3 = new Node<>(70);
+            node1.next = node2;
+            node2.next = node3;
+
+            Node<Integer> node = node1;
+            while (node != null) {
+                System.out.print(node.data+ " ");
+                node = node.next;
+            }
+        }
     }
-}
---
+++ src/main/java/example/Node.java
@@ -0,0 +1,13 @@
+package example;
+/*
+* using generics .
+* */
+public class Node<T> {
+    T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
